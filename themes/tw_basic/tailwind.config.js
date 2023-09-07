2f9ffe4fbc10f871cd7d521c8d108faa2bcb1902
RML <perrhh.comworld@gmail.com>
2023-09-07 21:35:51 +0200

    w: created first basic layout for the site

--- themes/tw_basic/tailwind.config.js
+++ themes/tw_basic/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      'font-family': {
+        body: [ "Helvetica Neue", "Helvetica", 'sans-serif', 'Arial' ],
+      }
+    },
   },
   plugins: [],
 }
